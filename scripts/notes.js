e1db515da6cd1141057c8e0f625e632d4af2b84b
Talia Glantz <taliaglantz13@gmail.com>
2021-10-07 16:37:15 +0100

    adding notes

---
+++ scripts/notes.js
@@ -0,0 +1,34 @@
+// player uses keys to guide object across the screen
+// key logic on grid that we learnt today
+
+// player to jump onto certain "safe paths" within grid
+// the would show the object on these paths
+
+// if player "misses" a path (e.g. because it is moving) or they misplace their step, they fall and lose a life
+// player falling into water (or something similar) would be when they miss a path i.e. when object does not successfully come into contact with good path
+
+// obstacles move horizonally (or vertically?!?!) across screen - timer funtion - how to do contact?
+// if player comes into contact with a moving obstacle, they lose a life
+
+// player wins by successfully moving object to far side of screen
+
+// players have certain number of lives until game is over
+
+// BONUS TASKS
+// players can win back lives by gaining certain objects
+// various different levels determined by speed of moving obstacles, size of screen et.
+//(can create a diagonal move??)
+
+// THEMES
+// baking/cooking - cooking mama style??
+// sweetie/chocolate world - Willy Wonka??
+// snow kingdom/ winter wonderland
+// crossing a road in NY with shopping bags/ crossing IKEA carpark/ millenial with plants
+
+
+// OTHER NOTES
+// I know this game
+// am ok with timers
+// can have fun with it and good practice with visuals, creativity etc.
+// All about collisions
+// Too much CSS? 
