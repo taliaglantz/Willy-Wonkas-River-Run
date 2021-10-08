21c10268ec7bf5a3da58875fc22b8856ba507d46
Talia Glantz <taliaglantz13@gmail.com>
2021-10-08 10:57:32 +0100

    updating10:57

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,34 +1,42 @@
 // player uses keys to guide object across the screen
-// key logic on grid that we learnt today
+//? key logic and boundaries on grid 
 
-// player to jump onto certain "safe paths" within grid
-// the would show the object on these paths
+// player to jump onto certain safe grounds
+//? certain grid boxes will always be "open" and players will be able to stay on them
+//? no bad assosciation with these boxes
 
-// if player "misses" a path (e.g. because it is moving) or they misplace their step, they fall and lose a life
-// player falling into water (or something similar) would be when they miss a path i.e. when object does not successfully come into contact with good path
+// if player misses an open box (e.g. maybe because it is moving?) or they misplace their step, they fall ( e.g. into the sea) and lose a life
+//? when object does not successfully come into contact with open box....
+//? ... it means they do come into contact with a "closed" box will...
+//? automatically illicit a reaction that makes player die/lose life
 
-// obstacles move horizonally (or vertically?!?!) across screen - timer funtion - how to do contact?
+// obstacles move horizonally  across screen
 // if player comes into contact with a moving obstacle, they lose a life
+//? timer function to move objects across screen --> CSS animations for movement?
+//? contact = when obstacle and object are in the same box at the same time 
 
 // player wins by successfully moving object to far side of screen
+//? when object manages to move to certain grid number
 
 // players have certain number of lives until game is over
+//? have 3 different "rounds", if player dies, move onto next round until die in round 3 and game over
 
 // BONUS TASKS
 // players can win back lives by gaining certain objects
-// various different levels determined by speed of moving obstacles, size of screen et.
+//? if certain object appears and player makes contact, player moves back a round
+// various different levels
+//? same game, but obstacles move quicker and size of grid is larger so more obstacles and safe grounds
+// player can gain points along the way
+//? by interacting with certain objects, player gets more points
 //(can create a diagonal move??)
+//? holding down the up and left/right arrow keys to move diagonally
+// obstacles moving vertically across screen - "cheat" move
+//? once object is in same box at same time as these, the obstacle "carries" the object to the top of the gric
+// player is able to choose what object they are
+
 
 // THEMES
 // baking/cooking - cooking mama style??
 // sweetie/chocolate world - Willy Wonka??
 // snow kingdom/ winter wonderland
 // crossing a road in NY with shopping bags/ crossing IKEA carpark/ millenial with plants
-
-
-// OTHER NOTES
-// I know this game
-// am ok with timers
-// can have fun with it and good practice with visuals, creativity etc.
-// All about collisions
-// Too much CSS? 
